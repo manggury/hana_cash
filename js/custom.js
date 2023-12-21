fc6dbb3c28b7137109078cdff6f9d5b16a2b2669
manggury <148739312+manggury@users.noreply.github.com>
2023-12-21 18:14:38 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,13 @@
-$(function(){
-    $('.visual_slide').slick({
-        arrows:false,
+$(function () {
+    $('.visual_slide_box').slick({
+        arrows: false,
+    });
+
+    $('.car_right_slide').slick({
+        arrows: false,
+    });
+
+    $('.event_slide').slick({
+        arrows: false,
     });
 })
